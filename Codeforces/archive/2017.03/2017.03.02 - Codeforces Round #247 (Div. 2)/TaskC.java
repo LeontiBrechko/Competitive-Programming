1bb153549884b804020c8e84cf0c81157b074e97
LeontiBrechko <leontibrechko@gmail.com>
2017-03-05 13:44:27 -0500

    3 dp problems

---
+++ Codeforces/archive/2017.03/2017.03.02 - Codeforces Round #247 (Div. 2)/TaskC.java
@@ -0,0 +1,37 @@
+package tasks;
+
+import lib.util.InputReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+@SuppressWarnings("Duplicates")
+public class TaskC {
+    private static final int MOD = 1000000007;
+
+    int n, k, d;
+    int[][] dp;
+    public void solve(int testNumber, InputReader in, PrintWriter out) {
+        n = in.nextInt();
+        k = in.nextInt();
+        d = in.nextInt();
+        dp = new int[n + 1][2];
+        for (int i = 0; i <= n; i++) Arrays.fill(dp[i], -1);
+        out.print(stepInto(0, false));
+    }
+
+    private int stepInto(int sum, boolean doesHaveD) {
+        if (sum == n) {
+            if (doesHaveD) return 1;
+            else return 0;
+        }
+        if (sum > n) return 0;
+        if (dp[sum][doesHaveD ? 1 : 0] != -1) return dp[sum][doesHaveD ? 1 : 0];
+
+        int res = 0;
+        for (int i = 1; i <= k; i++) {
+            res += stepInto(sum + i, doesHaveD || i >= d);
+            if (res >= MOD) res -= MOD;
+        }
+        return dp[sum][doesHaveD ? 1 : 0] = res;
+    }
+}
---
+++ Codeforces/archive/2017.03/2017.03.03 - tryenirovka-spbgu-s-5-dp-1/Longpath.java
@@ -0,0 +1,50 @@
+package tasks;
+
+import lib.util.InputReader;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@SuppressWarnings("Duplicates")
+public class Longpath {
+    private static final int MAX = 100001;
+
+    int n, m;
+    ArrayList<Integer>[] adj;
+    ArrayDeque<Integer> topology;
+    int[] dp;
+    boolean[] isVisited;
+
+    public void solve(int testNumber, InputReader in, PrintWriter out) {
+        n = in.nextInt();
+        m = in.nextInt();
+        adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+        for (int i = 0; i < m; i++) adj[in.nextInt() - 1].add(in.nextInt() - 1);
+
+        topology = new ArrayDeque<>();
+        isVisited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!isVisited[i]) dfs(i);
+        }
+
+        dp = new int[n];
+        for (Integer u : topology) {
+            for (Integer v : adj[u]) {
+                if (dp[u] + 1 > dp[v]) dp[v] = dp[u] + 1;
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < n; i++) res = Math.max(res, dp[i]);
+        out.print(res);
+    }
+
+    private void dfs(int u) {
+        isVisited[u] = true;
+        for (Integer v : adj[u]) if (!isVisited[v]) dfs(v);
+        topology.push(u);
+    }
+
+}
